f0eaa04d28d9f913533dd76db69d941d85561aaf
EbrarAkyildiz <akyildizebraresma0@gmail.com>
2026-01-13 15:53:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-document.getElementById("start").onclick = () => {
-  gpsAR.start({
-    lat: 41.017040,
-    lon: 28.858800,
-    model: "models/bina.glb",
-    scale: 0.3,
-    minAccuracy: 18,
-    altitude: 84
-  });
+import { iosAR } from "./ios-ar.js";
+
+const btn = document.getElementById("startBtn");
+const ui = document.getElementById("ui");
+
+btn.onclick = async () => {
+  navigator.geolocation.getCurrentPosition(
+    async (pos) => {
+
+      await iosAR.start({
+        lat: 41.017040,       // BİNA KOORDİNATI
+        lon: 28.858800,
+        model: "bina.glb",
+        scale: 1,
+        altitude: 0,
+        userLat: pos.coords.latitude,
+        userLon: pos.coords.longitude
+      });
+
+      ui.style.display = "none";
+    },
+    e => alert("GPS HATASI"),
+    { enableHighAccuracy: true }
+  );
 };
